a2b8a005ca9402ac9c0d7c04be700dd2db7691dc
Kredns <lucasbmccoy@gmail.com>
2015-12-20 15:09:44 -0500

    Started a very simple to do list.

---
+++ todo-list/todo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# My solution to the following challenge: https://redd.it/39ws1x
+
+from datetime import date
+
+class ToDo():
+	def __init__(self, date=date.today()):
+		self.items = []
+		self.date = date
+
+	def addItem(self, item):
+		if not item:
+			print("You cannot add a blank item.")
+			return
+
+		self.items.append(item)
+	
+	def removeItem(self, item):
+		if item in self.items:
+			self.items.remove(item)
+	
+	def print(self):
+		print("To Do List for {0}".format(self.date))
+		print(self.items)
+
+if __name__ == '__main__':
+	today = ToDo()
+	today.addItem('Take a shower.')
+	today.addItem('Go to work.')
+	today.addItem('') # Try to add a blank item.
+	today.print()
+	today.removeItem('Go to work.')
+	today.print()
