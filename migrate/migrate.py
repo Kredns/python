312d43b83fcba172c2157f1c29883c8cf76ce252
Kredns <lucasbmccoy@gmail.com>
2016-02-09 21:10:45 -0500

    Initial commit of migrate.py

---
+++ migrate/migrate.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+# This program is meant to allow the copying of files from RHEL 7 onto Windows
+#
--- todo-list/todo.py
+++ todo-list/todo.py
@@ -1,33 +1,29 @@
 #!/usr/bin/env python3
 # My solution to the following challenge: https://redd.it/39ws1x
 
+# I'm probably going to have to redo this whole thing to use a dictornary.
+
 from datetime import date
 
 class ToDo():
-	def __init__(self, date=date.today()):
+	def __init__(self):
 		self.items = []
-		self.date = date
 
-	def addItem(self, item):
+	def addItem(self, item, category='default'):
 		if not item:
 			print("You cannot add a blank item.")
 			return
 
-		self.items.append(item)
+		self.items.append([item, category])
 	
 	def removeItem(self, item):
+		# This has got to be fixed to accomdate for categories.
 		if item in self.items:
 			self.items.remove(item)
 	
 	def list(self):
-		print("To Do List for {0}".format(self.date))
-		print(self.items)
+		for item in self.items:
+			print(self.items)
 
 if __name__ == '__main__':
-	today = ToDo()
-	today.addItem('Take a shower.')
-	today.addItem('Go to work.')
-	today.addItem('') # Try to add a blank item.
-	today.list()
-	today.removeItem('Go to work.')
-	today.list()
+	pass	
