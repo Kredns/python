b77a66f83d65b21ffc714806e52a20fa6f2b7727
Lucas McCoy <lucasbmccoy@gmail.com>
2016-03-14 22:29:26 -0400

    Added colors, logging, and the option to specify a user from the command line to skip prompts.

--- migrate/migrate.py
+++ migrate/migrate.py
@@ -4,30 +4,46 @@ import os
 import sys
 import shutil
 import argparse
+import logging
 
+class Color:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
 
 class WindowsMigrate:
-    def __init__(self):
+    def __init__(self, log=False):
+        self.log = log
+        if log:
+            logging.basicConfig(filename='output.log', format='%(levelname)s:%(message)s')
         self.changed = 0
         self.home = ''
         self.path = ''
-        self.valid_chars = "&-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        self.valid_chars = "!@#$%^~`&-_.() abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
     def check_username(self, username):
         if not username:
-            print('[ERROR]: You must enter a username!')
+            if log:
+                logging.debug('User did not enter a username.')
+            print(Color.ERROR + '[ERROR]: You must enter a username!' + Color.ENDC)
             return False
 
         if os.path.exists('/home/' + username):
             self.home = '/home/' + username
             return True
         else:
-            print('[ERROR]: User not found')
+            if log:
+                logging.debug('Username that user enter was not found in /home.')
+            print(Color.ERROR + '[ERROR]: User not found'+ Color.ENDC)
             return False
 
     def initial_cleanup(self):
         # The first thing we need to do is delete .macromedia as it is not needed and
         # usually contains file paths longer than 260 characters.
+        print(Color.WARNING + 'Deleting ~/.macromedia and ~/.cache/mozilla/firefox!' + Color.ENDC)
         shutil.rmtree(self.home + '/.macromedia', ignore_errors=True)
         shutil.rmtree(self.home + '/.cache/mozilla/firefox', ignore_errors=True)
 
@@ -54,7 +70,9 @@ class WindowsMigrate:
             for name in files:
                 if len(name) > 255:
                     # TODO: Truncate filename.
-                    print('File {0} needs to be shortened.'.format(name))
+                    if self.log:
+                        log.warning('File {0} needs to be shortened!'.format(path + name))
+                    print(Color.WARNING + 'File {0} needs to be shortened.'.format(name) + Color.ENDC)
 
                 # Create a copy of the filename to work with. Next we grab the file extension
                 # for use later on. Then we remove any invalid characters.
@@ -64,11 +82,16 @@ class WindowsMigrate:
 
                 try:
                     if name != (new_name + ext):
+                        if self.log:
+                            log.info('Renaming file {old} to {new}{ext}'.format(old=self.path + name, new=new_name, ext=ext))
                         print('Renaming file {old} to {new}{ext}.'.format(old=name, new=new_name, ext=ext))
                         new_name = self.check_dupes(new_name, ext)
                         os.rename(self.path + name, self.path + new_name + ext)
                         self.changed += 1
                 except OSError as e:
+                    if self.log:
+                        log.debug('Failed to rename: {0} Was trying to use {1} Error message {2}'.format(self.path + name, 
+                            new_name, e))
                     print('Unable to rename file {0}.'.format(name))
                     print(e)
 
@@ -79,12 +102,17 @@ class WindowsMigrate:
                 new_dir = self.trim_invalid_chars(directory)
                 try:
                     if new_dir != directory:
+                        if self.log:
+                            log.info('Renaming directory {0} to {1}'.format(self.path + directory, new_dir))
                         print('Renaming directory {0} to {1}'.format(directory, new_dir))
                         new_dir = self.check_dupes(new_dir)
                         os.rename(self.path + directory, self.path + new_dir)
                         self.changed += 1
                 except OSError as e:
-                    print('Unable to rename directory {0}.'.format(directory))
+                    if self.logg:
+                        log.debug('Failed to rename directory: {0} Was trying to use: {1} Error message {2}'.format(
+                            self.path + directory, name, e))
+                    print(Color.ERROR + '[ERROR]: Unable to rename directory {0}.'.format(directory) + Color.ENDC)
                     print(e)
 
     def results(self):
@@ -93,19 +121,27 @@ class WindowsMigrate:
 def main():
     parser = argparse.ArgumentParser(description='Prep files to be moved to Windows from *nix.')
     parser.add_argument('--debug', '-d', action='store_true', help='debug mode is used for testing this script')
+    parser.add_argument('--log', action='store_true', help='enable logging, output is saved to output.log')
+    parser.add_argument('--user', '-u', nargs='?', help='allows specifying the user to skip the intro')
     args = parser.parse_args()
 
-    migration = WindowsMigrate()
+    if args.log:
+        migration = WindowsMigrate(log = True)
+    else:
+        migration = WindowsMigrate()
 
     if args.debug:
         migration.home = os.path.expanduser('~') + '/test_data'
         migration.fix_filenames()
         sys.exit(0)
 
-    print('Welcome to the Windows Migrate tool. This program will rename folders and files')
-    print('so that they can be moved to Windows without causing issues due to illegal')
-    print('characters or paths that are too long.\n')
-    username = input('Please enter the username of the user who you are migrating: ')
+    if args.user:
+        username = user
+    else:
+        print('Welcome to the Windows Migrate tool. This program will rename folders and files')
+        print('so that they can be moved to Windows without causing issues due to illegal')
+        print('characters or paths that are too long.\n')
+        username = input('Please enter the username of the user who you are migrating: ')
 
     success = migration.check_username(username)
     if not success:
