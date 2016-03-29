eb4db4800467838a89896d99d67692124c03fe3e
Kredns <lucasbmccoy@gmail.com>
2016-03-29 18:18:44 -0400

    Added main.py for fileman.

---
+++ fileman/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import sys
+
+if __name__ == '__main__':
+    print('Fileman is a CLI based file browser.')
+    print('It will use curses and try to be similar to the Superhot main menu.')
+    sys.exit(0)
