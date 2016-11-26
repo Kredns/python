fc6c6c3a5a50b776080b02d4e3b64de293bfb8a3
Kredns <lucasbmccoy@gmail.com>
2016-11-25 20:10:20 -0500

    Going with a defaultdict as of now.

--- todo-list/todo.py
+++ todo-list/todo.py
@@ -2,26 +2,31 @@
 # My solution to the following challenge: https://redd.it/39ws1x
 
 from datetime import date
+from collections import defaultdict
 
 class Todo:
     def __init__(self):
-        self.items = {}
+        self.items = defaultdict(list)
 
     def add_item(self, item, tag):
-        self.items[tag] = item
+        self.items[tag].append(item)
 
     def remove_item(self, item, tag):
-        pass
+        self.items[tag].remove(item)
 
     def print_all_items(self):
-        for item in self.items:
-            pass
-
-    def __str__(self):
-        return str(self.items)
+        for (tag, items) in self.items.items():
+            print str(tag) + ':'
+            for item in items:
+                print ' ' + item
+            print ''
         
 if __name__ == '__main__':
     todo = Todo()
     # I actually need to do this, for real.
     todo.add_item('Get an oil change.', 'Car')
-    print todo
+    todo.add_item('Plastidip my wheels.', 'Car')
+    todo.add_item('Clean my room.', 'Housework')
+    todo.print_all_items()
+    todo.remove_item('Get an oil change.', 'Car')
+    todo.print_all_items()
