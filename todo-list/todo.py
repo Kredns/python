2e3d133874e1df647df146ce90e0f4e2ccf84ef4
Kredns <lucasbmccoy@gmail.com>
2016-11-26 10:50:37 -0500

    Add and remove now work with minimal error checking.

--- todo-list/todo.py
+++ todo-list/todo.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 # My solution to the following challenge: https://redd.it/39ws1x
 
+import os
 from datetime import date
 from collections import defaultdict
+
+home = os.path.expanduser('~')
 
 class Todo:
     def __init__(self):
         self.items = defaultdict(list)
 
+    def __load_items(self):
+        try:
+            with open(home + '/.config/todo/list', 'r') as todo:
+                for item in todo.readline():
+                    # TODO: Need to figure out a way to store tags and items.
+                    pass
+        except IOError:
+            print 'You do not have any items to load.'
+
+    def __save_items(self):
+        try:
+            with open(home + '/.config/todo/list', 'w') as todo:
+                # TODO: Implement saving items.
+                pass
+
     def add_item(self, item, tag):
         self.items[tag].append(item)
 
     def remove_item(self, item, tag):
-        self.items[tag].remove(item)
+        if tag in self.items:
+            if item in self.items[tag]:
+                self.items[tag].remove(item)
+            else:
+                print "Item %s could not be found." % item
+        else:
+            print 'There is not tag named %s' % tag
 
     def print_all_items(self):
         for (tag, items) in self.items.items():
@@ -29,4 +53,6 @@ if __name__ == '__main__':
     todo.add_item('Clean my room.', 'Housework')
     todo.print_all_items()
     todo.remove_item('Get an oil change.', 'Car')
+    todo.remove_item('x', 'x')
+    todo.remove_item('x', 'Housework')
     todo.print_all_items()
