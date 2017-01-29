416f09ffb5f53a9d9aee27a393405af0b64ea158
Lucas McCoy <lmccoy@redhat.com>
2017-01-29 06:50:48 -0500

    Moved fileman into a class to do away with globals.

--- fileman/main.py
+++ fileman/main.py
@@ -2,37 +2,43 @@
 import os, sys
 import curses
 
-def show_files(screen, path):
-    row = 0
-    col = 1
-    for filename in os.listdir(path):
-        if row is 1 and col is 1:
-            screen.addstr(row, col, filename, curses.color_pair(1))
-        else:
-            screen.addstr(row, col, filename)
-        row = row + 1
-        if row == MAX_ROWS - 1:
-            row = 1
-            col = col + 30
-    screen.refresh()
+class fileman(object):
+    def __init__(self, screen):
+        self.screen = screen
+        self.MAX_ROWS, self.MAX_COLS = screen.getmaxyx()
+        self.screen.clear()
+        self.screen.border(0)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
 
-def main(screen):
-    global MAX_ROWS, MAX_COLS
-    MAX_ROWS, MAX_COLS = screen.getmaxyx()
-    screen.clear()
-    screen.border(0)
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
-    path = sys.argv[1]
-    show_files(screen, path)
-    opt = screen.getch()
-    curses.endwin()
+    def show_files(self, path):
+        row = 1
+        col = 1
+        for filename in os.listdir(path):
+            if row is 1 and col is 1:
+                self.screen.addstr(row, col, filename, curses.color_pair(1))
+            else:
+                self.screen.addstr(row, col, filename)
+            row = row + 1
+            if row == self.MAX_ROWS - 1:
+                row = 1
+                col = col + 30
+        self.screen.refresh()
+        opt = screen.getch()
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        path = os.getcwd()
+    else:
+        path = sys.argv[1]
+
     try:
         screen = curses.initscr()
         curses.start_color()
-        main(screen)
+        window = fileman(screen)
+        window.show_files(path)
     except Exception as e:
         print e
+        curses.endwin()
+    finally:
         curses.endwin()
 
