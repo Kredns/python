a432ed2d17428a72ccc6596b8ee2f02f4e31856f
Lucas <lucasbmccoy@gmail.com>
2017-02-04 10:54:24 -0500

    A very basic implementation of what I'm looking to do.

--- trello-todo/README.md
+++ trello-todo/README.md
@@ -1,3 +1,3 @@
 #trello-todo
----
+
 This is going to be a simple script that reads a python program and looks for # TODO: Once it finds that it adds the TODO to a trello card under the TODO board.
--- trello-todo/main.py
+++ trello-todo/main.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+# TODO: Read this line and add it to a card.
+# Anything below the first line should be added as a comment.
+
+#todo: Another test
+
+# TODO yet another test
+
+#TODO: Last test
+
+# This should not be added.
+def main():
+    with open('main.py', 'r') as f:
+        for line in f.readlines():
+            if (line.upper().startswith('# TODO:') or 
+                line.upper().startswith('#TODO:') or 
+                line.upper().startswith('#TODO')):
+                print line[7:].strip('#TODO: ')
+
 if __name__ == '__main__':
-    pass
+    try:
+        main()
+    except KeyboardInterrupt:
+        print
+        sys.exit(0)
