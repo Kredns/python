72446cd6bbdc0b0df5298dca4c7a5e8a6132fd03
Lucas <lucasbmccoy@gmail.com>
2017-02-04 11:12:53 -0500

    Moved test data into another file for cleanup.

--- trello-todo/main.py
+++ trello-todo/main.py
@@ -3,23 +3,24 @@
 import os
 import sys
 
-# TODO: Read this line and add it to a card.
-# Anything below the first line should be added as a comment.
-
-#todo: Another test
-
-# TODO yet another test
-
-#TODO: Last test
-
-# This should not be added.
 def main():
-    with open('main.py', 'r') as f:
-        for line in f.readlines():
+    with open('test_todos.py', 'r') as f:
+        # We do not care about the first line of the file because that should
+        # be a shebang.
+        f.readline()       
+        is_comment = False
+        for line in f:
             if (line.upper().startswith('# TODO:') or 
                 line.upper().startswith('#TODO:') or 
-                line.upper().startswith('#TODO')):
+                line.upper().startswith('#TODO') or
+                line.upper().startswith('# TODO')):
                 print line[7:].strip('#TODO: ')
+                is_comment = True
+            else:
+                if line.startswith('#'):
+                    print line[2:].strip('# ')
+                else:
+                    is_comment = False
 
 if __name__ == '__main__':
     try:
---
+++ trello-todo/test_todos.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# TODO: Read this line and add it to a card.
+# Anything below the first line should be added as a comment.
+
+#todo: Another test
+
+# TODO yet another test
+
+#TODO: Last test
+
+# This should not be added.
