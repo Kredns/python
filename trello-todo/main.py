4a287f78e3e599d35544213ccda0901717182388
Kredns <lucasbmccoy@gmail.com>
2017-02-14 14:43:44 -0500

    Trying another Trello API.

--- trello-todo/main.py
+++ trello-todo/main.py
@@ -3,16 +3,22 @@
 import os
 import re
 import sys
-from trello import *
+from trello import TrelloClient
+
+def strip_prefixes(string, prefixes):
+    for prefix in prefixes:
+        if string.startswith(prefix):
+            return string[len(prefix):].lstrip()
+    return string
 
 def find_todos(program):
     """Finds any occurrences of TODO and returns the entire line."""
     lines = None
+    # I know I could use a better regular expressiosn to get the perfect match
+    # and remove the TODO's, but I'd rather do that via python instead of some
+    # insane RegEx that I won't be able to understand a week from now.
     pattern = r'((TODO|todo).*)'
     with open(program, 'r') as f:
-        # We do not care about the first line of the file because that should
-        # be a shebang.
-        f.readline()       
         lines = f.readlines()
 
     for line in lines:
@@ -22,16 +28,24 @@ def find_todos(program):
 
 def main(filename):
     todos = find_todos(filename)
+    prefixes = ['TODO:', 'TODO: ', 'todo:', 'todo: ', 'TODO ', 'todo ']
     for todo in todos:
-        print todo
+        print strip_prefixes(todo, prefixes)
 
-    TRELLO_APP_KEY = None
-    AUTH_TOKEN = None
-    with open('TRELLO_APP_KEY', 'r') as k:
-        TRELLO_APP_KEY = k.readline()
-    with open('AUTH_TOKEN', 'r') as t:
-        AUTH_TOKEN = t.readline()
-    trello = TrelloApi(TRELLO_APP_KEY, AUTH_TOKEN)
+    #TRELLO_APP_KEY = None
+    #AUTH_TOKEN = None
+    #with open('TRELLO_APP_KEY', 'r') as k:
+    #    TRELLO_APP_KEY = k.readline().strip()
+    #with open('AUTH_TOKEN', 'r') as t:
+    #    AUTH_TOKEN = t.readline().strip()
+
+    #client = TrelloClient(TRELLO_APP_KEY, token=AUTH_TOKEN)
+    #board = client.get_board('6yIqXMb5')
+    #lists = board.get_lists('all')
+    #todo_list = lists[0]
+
+    #for todo in todos:
+    #    todo_list.add_card(todo) 
 
 if __name__ == '__main__':
     try:
