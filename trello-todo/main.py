cc552e68988a078930907b8a0d5c8520b24bdaf8
Kredns <lucasbmccoy@gmail.com>
2017-02-16 16:25:30 -0500

    Moved code into a class. Working on a basic model of how this should work.

--- trello-todo/main.py
+++ trello-todo/main.py
@@ -5,47 +5,52 @@ import re
 import sys
 from trello import TrelloClient
 
-def strip_prefixes(string, prefixes):
-    for prefix in prefixes:
-        if string.startswith(prefix):
-            return string[len(prefix):].lstrip()
-    return string
+class TrelloTodo(object):
+    def __init__(self, API_KEY, AUTH_TOKEN, board):
+        self.prefixes = ['TODO:', 'TODO: ', 'todo:', 'todo: ', 'TODO ', 'todo ']
+        self.client = TrelloClient(API_KEY, token=AUTH_TOKEN)
+        self.board = self.client.get_board(board)
+        lists = self.board.get_lists('all')
+        # In order for this to work TODO has to be the first list on the board.
+        lists.reverse()
+        self.todo = lists.pop()
 
-def find_todos(program):
-    """Finds any occurrences of TODO and returns the entire line."""
-    lines = None
-    # I know I could use a better regular expressiosn to get the perfect match
-    # and remove the TODO's, but I'd rather do that via python instead of some
-    # insane RegEx that I won't be able to understand a week from now.
-    pattern = r'((TODO|todo).*)'
-    with open(program, 'r') as f:
-        lines = f.readlines()
+    def __strip_prefixes(self, string, prefixes):
+        for prefix in prefixes:
+            if string.startswith(prefix):
+                return string[len(prefix):].lstrip()
+        return string
 
-    for line in lines:
-        match = re.search(pattern, line)
-        if match:
-            yield match.group()
+    def __archive_all_cards(self):
+        pass
+
+    def find_todos(self, program):
+        """Finds any occurrences of TODO and returns the entire line."""
+        lines = None
+        # I know I could use a better regular expressiosn to get the perfect match
+        # and remove the TODO's, but I'd rather do that via python instead of some
+        # insane RegEx that I won't be able to understand a week from now.
+        pattern = r'((TODO|todo).*)'
+        with open(program, 'r') as f:
+            lines = f.readlines()
+
+        for line in lines:
+            match = re.search(pattern, line)
+            if match:
+                yield match.group()
 
 def main(filename):
-    todos = find_todos(filename)
-    prefixes = ['TODO:', 'TODO: ', 'todo:', 'todo: ', 'TODO ', 'todo ']
-    for todo in todos:
-        print strip_prefixes(todo, prefixes)
+    API_KEY = None
+    AUTH_TOKEN = None
+    with open('API_KEY', 'r') as k:
+        API_KEY = k.readline().strip()
+    with open('AUTH_TOKEN', 'r') as t:
+        AUTH_TOKEN = t.readline().strip()
 
-    #TRELLO_APP_KEY = None
-    #AUTH_TOKEN = None
-    #with open('TRELLO_APP_KEY', 'r') as k:
-    #    TRELLO_APP_KEY = k.readline().strip()
-    #with open('AUTH_TOKEN', 'r') as t:
-    #    AUTH_TOKEN = t.readline().strip()
-
-    #client = TrelloClient(TRELLO_APP_KEY, token=AUTH_TOKEN)
-    #board = client.get_board('6yIqXMb5')
-    #lists = board.get_lists('all')
-    #todo_list = lists[0]
-
-    #for todo in todos:
-    #    todo_list.add_card(todo) 
+    board_id = '6yIqXMb5'
+    todo = TrelloTodo(API_KEY, AUTH_TOKEN, board_id)
+    for item in todo.find_todos(filename):
+        print item
 
 if __name__ == '__main__':
     try:
