d3c7b8990722cca9a9016f30da259a9cd0d7eec4
Kredns <lucasbmccoy@gmail.com>
2017-02-23 10:40:45 -0500

    Forgot a comma.

--- snippet/setup.py
+++ snippet/setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='snippet',
       version='1.0',
       description='Snippet copies text snippets into your clipboard and replaces predefined variables',
-      author='Lucas McCoy'
+      author='Lucas McCoy',
       author_email='lmccoy@redhat.com',
       packages['snippet'],
      )
