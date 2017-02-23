74fd5e2e834f90cd5e6481486f71b1630e9e495c
Kredns <lucasbmccoy@gmail.com>
2017-02-23 11:03:24 -0500

    Still testing a setup.py

--- snippet/setup.py
+++ snippet/setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='snippet',
       version='1.0',
       description='Snippet copies text snippets into your clipboard and replaces predefined variables',
       author='Lucas McCoy',
       author_email='lmccoy@redhat.com',
-      packages=['snippet'],
+      packages=find_packages(),
      )
