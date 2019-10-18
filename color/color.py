b078f7e36981e24c4ae9dab5e0cf5b9d8a5b7e71
Lucas McCoy <lucasbmccoy@gmail.com>
2019-10-18 14:06:17 -0400

    Updated the color class to Python 3 and fixed shit!

--- color/color.py
+++ color/color.py
@@ -1,17 +1,34 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 class Color:
-    WARNING = '\033[96m'
-    ERROR = '\033[91m'
-    ENDC = '\033[0m'
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
     RED = '\033[91m'
-    REDB = '\033[41m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m' 
-    GREEN = '\033[32m' 
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
 
     @staticmethod
-    def colorize(text, color, use_color=True):
+    def apply(text, color, use_color=True):
         if use_color is False:
             return text
-        return color + text + Color.ENDC
+        return color + text + Color.END
+
+def display_colors():
+    print(Color.apply('This should be purple!', Color.PURPLE))
+    print(Color.apply('This should be cyan!', Color.CYAN))
+    print(Color.apply('This should be dark cyan (teal?)!', Color.DARKCYAN))
+    print(Color.apply('This should be blue!', Color.BLUE))
+    print(Color.apply('This should be green!', Color.GREEN))
+    print(Color.apply('This should be yellow!', Color.YELLOW))
+    print(Color.apply('This should be red!', Color.RED))
+    print(Color.apply('This should be bold!', Color.BOLD))
+    print(Color.apply('This should be underline!', Color.UNDERLINE))
+
+
+if __name__ == '__main__':
+    display_colors()
